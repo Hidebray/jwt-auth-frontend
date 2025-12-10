8813dea1ce2f8284683f39ed41655fab63bd8acb
Hidebray <daihiep10092005@gmail.com>
2025-12-10 19:17:20 +0700

    Config enviroment

--- src/api/axiosClient.ts
+++ src/api/axiosClient.ts
@@ -1,7 +1,7 @@
 // client/src/api/axiosClient.ts
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:5000/api'; // Trỏ thẳng về Server thật
+const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
 
 // Biến lưu Access Token trong bộ nhớ
 let accessToken: string | null = null;
